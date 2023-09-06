7503c752af2cc811bd5aacdbd521db9228475fa5
AngryMexDog <aortiz19.depad.sj@gmail.com>
2023-09-05 20:03:03 -0600

    Create hard generator

--- main.py
+++ main.py
@@ -9,15 +9,33 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #EASY LEVEL
-password = ""
+#password = ""
+#for char in range(1, nr_letters + 1):
+#    password += random.choice(letters)
+#for char in range(1, nr_symbols + 1):
+#    password += random.choice(symbols)
+#for char in range(1, nr_numbers + 1):
+#    password += random.choice(numbers)
+#print(password)
+
+#Hard Level
+password_list = []
 
 for char in range(1, nr_letters + 1):
-    password += random.choice(letters)
+  password_list += random.choice(letters)
 
 for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
+  password_list.append(random.choice(symbols))
 
 for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+  password_list.append(random.choice(numbers))
 
-print(password)
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
